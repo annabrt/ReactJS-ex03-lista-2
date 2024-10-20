77d90918ca372e78adc17fa3e33d78fae1cf09cb
Anna Bertochi <anafbrtc@gmail.com>
2024-10-20 01:18:14 -0300

    Add files via upload

---
+++ ex03l2/src/App.jsx
@@ -0,0 +1,10 @@
+import Lista from './components/Lista'
+import './App.css'
+
+function App() {
+  return (
+    <Lista/>
+  );
+}
+
+export default App
---
+++ ex03l2/src/components/Lista.jsx
@@ -0,0 +1,13 @@
+function Lista() {
+    return (
+      <h1>
+        <ol>
+        <li>  Estudar React</li>
+        <li>  Praticar JavaScript</li>
+        <li>  Fazer exercícios</li>
+      </ol>
+      </h1>
+    );
+  }
+  
+  export default Lista;
